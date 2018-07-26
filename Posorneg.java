a13a2603234d97b227dc37be1639841824df487c
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-07-26 15:31:47 +0530

    Create Posorneg.java

---
+++ Posorneg.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Posorneg
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a==0)
+		System.out.println("zero");
+		else if(a>0)
+		System.out.println("positve");
+		else
+		System.out.println("negative");
+	}
+}
