09811a461f2eb869a51f29c28fc9cd75933c574c
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-07-30 10:36:47 +0530

    Create Sort.java

---
+++ Sort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a[]=new int[20];
+   int n,i,j,temp;
+     Scanner sc=new Scanner(System.in);
+     n=sc.nextInt();
+     for(i=0;i<n;i++)
+     {
+    	a[i]=sc.nextInt();
+     }
+    for(i=0;i<n;i++)
+    {
+    	for(j=i;j<n;j++)
+    		
+    	{
+    		if(a[i]>a[j])
+    		{
+    		    temp=a[j];
+    			a[j]=a[i];
+    			a[i]=temp;	
+    		}
+    	}
+    }
+   
+    for(i=0;i<n;i++)
+    {
+    	System.out.println(a[i]);
+    }
+	}
+
+}
