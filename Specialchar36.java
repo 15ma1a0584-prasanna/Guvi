b37d5b64ad3be8e9b04ca3964c44809d6b6560dc
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-10 19:46:35 +0530

    Update Specialchar36.java

--- Specialchar36.java
+++ Specialchar36.java
@@ -1,18 +1,23 @@
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
-class Spacialchar36{
-     public static void main(String aa[]){
-      String a;
-      int count=1;
-      Scanner s=new Scanner(System.in);
-      a=s.nextLine();
-     for(int i=0;i<a.length();i++){
-        if(a.charAt(i)>='0'&&a.charAt(i)<='9'){
-             count++;
-        }
-     }
-     System.out.println(count);
-    }
+import java.io.*;
 
-      
+/* Name of the class has to be "Main" only if the class is public. */
+class Specialchar36
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int ch=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(!(Character.isDigit(str.charAt(i)))&&!(Character.isAlphabetic(str.charAt(i))))
+			ch++;
+			
+		}
+		System.out.println(ch);
+	}
 }
