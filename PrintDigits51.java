2ec5f540cd376003a8c49a96fc5e1cf1e6fb3e20
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-12 21:45:27 +0530

    Create PrintDigits51.java

---
+++ PrintDigits51.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class PrintDigits51
+{
+	public static void printDigits51(int num) {
+    if (num >= 10) {
+        printDigits51(num / 10);
+    }
+    System.out.print(num % 10);
+    System.out.print(" ");
+}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		PrintDigits51 obj=new PrintDigits51();
+		obj.printDigits51(num);
+	}
+}
