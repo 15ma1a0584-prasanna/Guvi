865fed63fe49cb61df5d1b9423f18ad2cc5ba069
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-13 10:06:46 +0530

    Create NearestEvenNumber54.java

---
+++ NearestEvenNumber54.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class NearestEvenNumber54{
+     public static void main(String aa[]){
+     int n;
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     if(n%2!=0){
+         System.out.print(n-1);
+     }
+
+}
+}
