d9b0f35b58526614e9e6ff1fbd635ed9454c15fd
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-13 09:46:53 +0530

    Update NumberToWords52.java

--- NumberToWords52.java
+++ NumberToWords52.java
@@ -1,42 +1,19 @@
 import java.util.*;
-class NumberToWords52
-{
- 
-	public void pw(int n, String ch)
-	{
-		String one[] = { " ", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten",
-				" Eleven", " Twelve", " Thirteen", " Fourteen", "Fifteen", " Sixteen", " Seventeen", " Eighteen",
-				" Nineteen" };
- 
-		String ten[] = {" "," ","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
- 
-		if (n > 19)
-		{
-			System.out.print(ten[n / 10]+" "+one[n % 10]);
-		}
-		else
-		{
-			System.out.print(one[n]);
-		}
-		if (n > 0)
-			System.out.print(ch);
-	}
- 
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		
-		int n=sc.nextInt();
-	
-		if (n>0)
-		{
-			NumberToWords52 a = new NumberToWords52();
-			a.pw((n / 1000000000), " Hundred");
-			a.pw((n / 10000000) % 100, " crore");
-			a.pw(((n / 100000) % 100), " lakh");
-			a.pw(((n / 1000) % 100), " thousand");
-			a.pw(((n / 100) % 10), " hundred");
-			a.pw((n % 100), " ");
-		}
-	}
+import java.lang.*;
+import java.util.ArrayList;
+class NumberToWords52{
+     public static void main(String aa[]){
+     int a[]={1,2,3,4,5,6,7,8,9,10};
+     String c[]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+     int n;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     
+     for(int i=0;i<a.length;i++){
+         if(a[i]==n){
+             System.out.println(c[i]);
+         }
+     }
+
+}
 }
