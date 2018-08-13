dbbb7712cf24b315db1381e45e487fdc08e19563
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-13 13:56:50 +0530

    Create Prg58.java

---
+++ Prg58.java
@@ -0,0 +1,27 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Prg58
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int count=0;
+		int n=sc.nextInt(),k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			if(k==a[i])
+			count++;
+		}
+		if(count>0)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
