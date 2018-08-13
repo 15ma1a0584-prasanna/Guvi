739eb4d3abf898e2ab62b52cb36f739a86feb3db
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-13 22:30:16 +0530

    Create Min63.java

---
+++ Min63.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Min63 { 
+ 
+  public static void main(String args[])
+  {
+  	Scanner sc=new Scanner(System.in);
+   int array[] = new int[10];
+   for(int i=0;i<10;i++)
+   {
+   	array[i]=sc.nextInt();
+   }
+ 
+    int min = getMin(array);
+    System.out.println(min);
+  }
+  public static int getMin(int[] inputArray){ 
+    int minValue = inputArray[0]; 
+    for(int i=1;i<inputArray.length;i++){ 
+      if(inputArray[i] < minValue){ 
+        minValue = inputArray[i]; 
+      } 
+    } 
+    return minValue; 
+  } 
+}
