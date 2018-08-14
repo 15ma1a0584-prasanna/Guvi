8170f4f38371028d17fc3931f0ce6337c04c38f8
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-14 12:52:26 +0530

    Update Factor77.java

--- Factor77.java
+++ Factor77.java
@@ -1,22 +1,19 @@
 import java.util.*;
 import java.lang.*;
-import java.io.*;
-
-
+import java.util.ArrayList;
 class Factor77
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		for(int i=1;i<=n;i++)
-		{
-			if(n%i==0)
-			
-			System.out.print(i+" ");
-		}
-			
-		
-		
-	}
+     public static void main(String aa[]){
+       int b;
+        Scanner s=new Scanner(System.in);
+        b=s.nextInt();
+        for(int i=1;i<=b;i++){
+            if(b%i==0){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        }
+      
+  
 }
+    }
