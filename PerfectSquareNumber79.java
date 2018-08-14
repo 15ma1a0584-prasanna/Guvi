8a180619ac90e9188df644247e4c6c546ca987f9
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-14 16:34:44 +0530

    Create PerfectSquareNumber79.java

---
+++ PerfectSquareNumber79.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class PerfectSquareNumber79 {
+public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int number;
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        number=n*k;
+
+        int sqrt = (int) Math.sqrt(number);
+
+        if(sqrt*sqrt == number) {
+
+            System.out.println("yes");
+
+        }else {
+
+            System.out.println("no");
+
+        }
+
+         
+
+    }
+
+}
