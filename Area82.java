3b541dca1e68aa386b549dcac15736384dfa02e3
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-16 11:38:56 +0530

    Create Area82.java

---
+++ Area82.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Area82
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float n1=sc.nextFloat();
+		float n2=sc.nextFloat();
+		float n3;
+		n3=n1*n2;
+		System.out.println(n3);
+		
+	}
+}
