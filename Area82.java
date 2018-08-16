40540ac023c59182dff0f9030bee8684523615f2
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-16 12:22:53 +0530

    Update Area82.java

--- Area82.java
+++ Area82.java
@@ -1,19 +1,16 @@
-
-
+import java.text.DecimalFormat;
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Area82
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc=new Scanner(System.in);
-		float n1=sc.nextFloat();
-		float n2=sc.nextFloat();
-		float n3;
-		n3=n1*n2;
-		System.out.println(n3);
-		
-	}
+class DecimalFormatExample {   
+  
+    public static void main(String args[])  {
+    	Scanner sc=new Scanner(System.in);
+    	float n1=sc.nextFloat();
+    	float n2=sc.nextFloat();
+    	float n3;
+    	n3=n1*n2;
+        DecimalFormat df = new DecimalFormat("##0.00000");
+        System.out.println(df.format(n3));
+    }
+      
 }
