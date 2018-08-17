49fe6471c940b45daffdaa4f7c77268bfd2d4643
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-17 10:25:50 +0530

    Create KthNumber94.java

---
+++ KthNumber94.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class KthNumber94 {
+
+	public static void main(String[] args) {
+		int N,K,i;
+		Scanner sc=new Scanner(System.in);
+
+		N=sc.nextInt();
+		K=sc.nextInt();
+		int[] array=new int[N];
+	
+		for(i=0;i<N;i++)
+		{
+			array[i]=sc.nextInt();
+		}
+		System.out.println(array[K-1]);
+
+	}
+
+}
