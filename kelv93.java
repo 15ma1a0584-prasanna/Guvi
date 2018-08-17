277ee0ec5619f39724545ec90e215c504ed19f1b
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-17 10:17:24 +0530

    Create kelv93.java

---
+++ kelv93.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class kelv93{
+
+     public static void main(String aa[]){
+    int a,b,c;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    int n=a+273;
+    System.out.println(n);
+
+           
+   }
+}
