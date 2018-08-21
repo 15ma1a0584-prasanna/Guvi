0d4da7257b4d88de08670bb802fc65b37209111d
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-21 10:35:31 +0530

    Create Join105.java

---
+++ Join105.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Join105
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String n=sc.next();
+		String k=sc.next();
+		System.out.println(n+k);
+		
+	}
+}
