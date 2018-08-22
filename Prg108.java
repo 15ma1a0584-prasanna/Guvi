aee1a1b079585e5510fe3583660bc8d53458858f
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-22 10:31:14 +0530

    Create Prg108.java

---
+++ Prg108.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Prg108
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=1;i<n;i++)
+		a[i]=sc.nextInt();
+		System.out.println(a[k]);
+	}
+}
