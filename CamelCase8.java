5594319cf7b09234f6c89aee04e3e1ea6a4f4fce
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-23 10:21:27 +0530

    Update CamelCase8.java

--- CamelCase8.java
+++ CamelCase8.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
- class CamelCase8
+ class CamelCase
  {
  	public static void main(String[] args)
  	{
  		Scanner scanner = new Scanner(System.in);
                         String input = scanner.nextLine();
                         String result = "";
+                        input=input.toLowerCase();
                         char firstChar = input.charAt(0);
                         result = result + Character.toUpperCase(firstChar);
                         for (int i = 1; i < input.length(); i++)
