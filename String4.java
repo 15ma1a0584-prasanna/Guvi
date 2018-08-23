29d0af9a35b1f9e9d60ea0d227516ae7e0819a55
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-08-23 09:47:34 +0530

    Create String4.java

---
+++ String4.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class String4
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		StringBuffer sb=new StringBuffer();
+		String str=sc.nextLine();
+		sb.append(".");
+		System.out.println(str+sb.toString());
+	}
+}
