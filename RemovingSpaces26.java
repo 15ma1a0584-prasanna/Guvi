da8d1420734c59b0d4ef8ec81a830484883ac71d
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-09-04 11:58:19 +0530

    Update RemovingSpaces26.java

--- RemovingSpaces26.java
+++ RemovingSpaces26.java
@@ -1,10 +1,26 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 class RemovingSpaces26
 {
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		String str=s.nextLine();
-		String str1=str.trim().replaceAll(" +"," ");
-		System.out.println(str1);
-	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			String text;
+		Scanner sc=new Scanner(System.in);
+		text=sc.nextLine();
+if(text.indexOf("  ")>0){
+ while(text.indexOf("  ") >= 0)
+{
+	
+text = text.replaceAll("  "," ");
+}
+System.out.print(text);
+}
+
+else{
+	text=text.replaceAll(" ","");
+	System.out.print(text);
+}
+}
+	
 }
