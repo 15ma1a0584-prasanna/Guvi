e203ba8ca67704f21c32ca0ed19541987b5d2a71
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-09-28 19:17:38 +0530

    Create Strrot44.java

---
+++ Strrot44.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class Strrot44
+{
+    public static void main(String aa[]){
+    Scanner s=new Scanner(System.in);
+    String c=s.next();
+    int l=s.nextInt();
+    int n=c.length();
+    ArrayList<Character>z=new ArrayList<Character>();
+    for(int i=0;i<n;i++){
+        z.add(c.charAt(i));
+    }
+    Collections.rotate(z, l);
+    for(char cc:z){
+        System.out.print(cc);
+    }
+}
+}
