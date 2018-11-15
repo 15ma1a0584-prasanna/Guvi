0c433d6951d818f3f3d1e7ef8f49b670b88b6581
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-11-15 10:19:24 +0530

    Create EveryThirdCharacter34.java

---
+++ EveryThirdCharacter34.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+class EveryThirdCharacter34 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s=in.next();
+        if(s.length()==1)
+            System.out.println(s);
+        else{
+        for(int i=0;i<s.length();i+=3)
+        {
+            System.out.print(s.charAt(i));
+        }
+        }
+    }
+}
