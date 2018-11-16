4264c31cf879d5b1fb9ed3f7ee731152466a577e
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-11-16 19:55:43 +0530

    Create Players_prg63.java

---
+++ Players_prg63.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Players_prg63
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,t=0;
+	    n=sc.nextInt();
+	    
+	    int[] a=new int[n];
+	    int[] b=new int[n];
+	    
+	    for(int i=0;i<n;i++)
+	        a[i]=sc.nextInt();
+	    
+	     for(int i=0;i<n;i++)
+	        b[i]=sc.nextInt();
+	        
+	        for(int i=0;i<n;i++)
+	        {
+	            for(int j=0;j<n;j++)
+	            {
+	                if(a[i]==b[j] && a[i]!=t)
+	                {
+	                    System.out.print(a[i]+" ");
+	                    t=a[i];
+	                }
+	                
+	            }
+	        }
+    }
+}
