6d466801cc454ccef613018b5ceab94acc4d540e
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-11-18 16:52:42 +0530

    Create Players_prg74.java

---
+++ Players_prg74.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Players_prg74
+{
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int b=in.nextInt();
+	int c=(int)Math.pow(a, b);
+	System.out.println(c);
+}
+}
