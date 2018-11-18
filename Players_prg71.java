c0f5af6adcc3280f8ec5da173b19b5aac6965abf
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-11-18 16:22:26 +0530

    Create Players_prg71.java

---
+++ Players_prg71.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Players_prg71
+{
+  public static void main(String aa[]){
+      int n;
+      Scanner s=new Scanner(System.in);
+      n=s.nextInt();
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=s.nextInt();
+      }
+      for(int i=0;i<n-1;i++){
+          if(a[i]>a[i+1]){
+              System.out.print(a[i]+" ");
+          }
+ else{
+              System.out.print(a[i+1]+" ");
+ }
+      }
+    }
+}
