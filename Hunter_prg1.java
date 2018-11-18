5cb91cf7383f3dfc944998978540999b1cd7a4e3
15ma1a0584-prasanna <41730162+15ma1a0584-prasanna@users.noreply.github.com>
2018-11-18 17:47:49 +0530

    Update Hunter_prg1.java

--- Hunter_prg1.java
+++ Hunter_prg1.java
@@ -1,33 +1,75 @@
-import java.io.*;
 import java.util.*;
-class Hunter_prg1
+import java.lang.*;
+
+
+ class Hunter_prg1
 {
-public static void main(String args[])
-{
-int n;
-int v=0;
-Scanner sc=new Scanner(System.in);
-n=sc.nextInt();
-int a[]=new int[n];
-for(int i=0;i<n;i++)
-{
-a[i]=sc.nextInt();
-}
-Arrays.sort(a);
-for(int j=0;j<n;j++)
-{
-for(int k=j+1;k<n;k++)
-{
-if(a[j]==a[k])
-{
-v=1;
-System.out.print(a[j]+" ");
-}
-}
-}
-if(v==0)
-{
-System.out.println("Unique");
-}
-}
-}
+	static void numrepeatsort(int[] arr)
+	{
+		int i=arr.length;		
+		int t=0;
+		int a=0;
+		
+		while(a<i)
+		{
+			int flag=0;int k=0;
+			for(int j=a+1;j<arr.length;j++)
+			{
+				if(arr[a]==arr[j] && arr[a]!=-1)
+				{
+					flag=1;
+					for(k=j;k<arr.length;k++)
+					{
+						if(arr[k]==arr[a])
+							arr[k]=-1;
+					}
+					t++;	
+					break;
+				}
+			}			
+			if(flag==0)
+				arr[a]=-1;
+			a++;
+		}
+		
+		int[] sortedarr = new int[t];
+		int z=0;
+		for(int v: arr)
+		{			
+			if(v!=-1)
+			{
+				sortedarr[z]=v;
+			z++;
+			}
+		}		
+
+		for(int j=0;j<sortedarr.length-1;j++)
+		{
+			for(int k=j+1;k<sortedarr.length;k++)
+			{
+				if(sortedarr[k]<sortedarr[j])
+				{
+					int tempr=sortedarr[j];
+					sortedarr[j]=sortedarr[k];
+					sortedarr[k]=tempr;
+				}
+			}
+		}
+	
+	  for(int r: sortedarr)
+			System.out.print(r+" ");
+	}
+	public static void main(String[] args)
+	{
+		
+		Scanner sn = new Scanner(System.in);
+		int a=sn.nextInt();
+		int[] arr = new int[a];
+		while(--a>=0)
+		{			
+		    arr[a]=sn.nextInt();
+		}		
+		numrepeatsort(arr);		
+	}
+	
+	}
